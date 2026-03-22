269881c18f0a153af5fe56d37ea62112f5aa4563
Lin <tzu.hsun.lin046@gmail.com>
2026-03-22 13:54:05 +0800

    feat: add navigation to welcome page buttons

--- src/pages/guide/welcome.tsx
+++ src/pages/guide/welcome.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 import { PrimaryButton } from '@/modules/guide/components/PrimaryButton'
 
 const Welcome = () => {
+  const router = useRouter()
   return (
     <div className="flex w-full max-w-[375px] flex-col items-center justify-center gap-60 px-4 pt-16">
       <section className="flex w-full flex-col items-center gap-14">
@@ -12,7 +14,7 @@ const Welcome = () => {
         </section>
         <Image src="/guide/guide-welcome-character.webp" alt="welcome" width={180} height={180} />
       </section>
-      <PrimaryButton content="開始" />
+      <PrimaryButton content="開始" onClick={() => router.push('/guide/gender')} />
     </div>
   )
 }
