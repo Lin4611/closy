97064712a0c0003cd87d7af10a6384a17bcc876f
Lin <tzu.hsun.lin046@gmail.com>
2026-03-25 10:21:35 +0800

    feat: add placeholder pages

---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,13 @@
+import { AppShell } from '@/modules/common/components/AppShell'
+
+const Setting = () => {
+  return (
+    <AppShell>
+      <div className="flex h-screen w-full items-center justify-center">
+        <h1>Setting</h1>
+      </div>
+    </AppShell>
+  )
+}
+
+export default Setting
