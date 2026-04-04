fb9832fbb1dc14c7bea81cf2f310c42cdd8a3ef1
Lin <tzu.hsun.lin046@gmail.com>
2026-04-04 17:21:10 +0800

    refactor: rename data and types files for consistency

--- src/modules/home/components/QuickAdjustOptions.tsx
+++ src/modules/home/components/QuickAdjustOptions.tsx
@@ -1,11 +1,17 @@
 import { AdjustSuggestionChip } from './AdjustSuggestionChip'
-import { outfitAdjustPromptOptions } from '../data/OutfitAdjustOptions'
-
-export const QuickAdjustOptions = () => {
+import { outfitAdjustPromptOptions } from '../data/outfitAdjustOptions'
+type QuickAdjustOptionsProps = {
+  onSelectOption: (option: string) => void
+}
+export const QuickAdjustOptions = ({ onSelectOption }: QuickAdjustOptionsProps) => {
   return (
     <section className="flex flex-wrap justify-center gap-x-3 gap-y-4">
       {outfitAdjustPromptOptions.map((option) => (
-        <AdjustSuggestionChip key={option.id} label={option.label} />
+        <AdjustSuggestionChip
+          key={option.id}
+          label={option.label}
+          onClick={() => onSelectOption(option.label)}
+        />
       ))}
     </section>
   )
