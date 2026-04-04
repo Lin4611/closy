1fb5297a3b382b104c99e37e7a85f00471bbff9d
Lin <tzu.hsun.lin046@gmail.com>
2026-04-04 21:14:28 +0800

    feat: add outfit adjust chat message components

---
+++ src/modules/home/components/OutfitAdjustChat.tsx
@@ -0,0 +1,19 @@
+import { OutfitAdjustMessageBubble } from './OutfitAdjustMessageBubble'
+import type { OutfitAdjustChatMessage } from '../types/outfitAdjustChat'
+type OutfitAdjustChatProps = {
+  messages: OutfitAdjustChatMessage[]
+}
+export const OutfitAdjustChat = ({ messages }: OutfitAdjustChatProps) => {
+  return (
+    <div className="hide-scrollbar flex min-h-0 flex-1 flex-col gap-10 overflow-y-auto">
+      {messages.map((message) => (
+        <OutfitAdjustMessageBubble
+          key={message.id}
+          text={message.text}
+          role={message.role}
+          status={message.status}
+        />
+      ))}
+    </div>
+  )
+}
