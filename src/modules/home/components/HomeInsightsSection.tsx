332fe4b906695c8412bda9cccb335d3c7ffaad89
Lin <tzu.hsun.lin046@gmail.com>
2026-04-08 11:39:30 +0800

    feat: update onboarding flow and replace calendar card with action buttons

--- src/modules/home/components/HomeInsightsSection.tsx
+++ src/modules/home/components/HomeInsightsSection.tsx
@@ -1,4 +1,3 @@
-import { CalendarSummaryCard } from './CalendarSummaryCard'
 import { OutfitForecastCard } from './OutfitForecastCard'
 type HomeInsightsSectionProps = {
   content?: string
@@ -7,8 +6,7 @@ export const HomeInsightsSection = ({
   content = '您還未新增任何衣物',
 }: HomeInsightsSectionProps) => {
   return (
-    <section className="flex flex-col items-center justify-center gap-6 pt-7 pb-9">
-      <CalendarSummaryCard />
+    <section className="flex flex-col items-center justify-center pt-7 pb-9">
       <OutfitForecastCard content={content} />
     </section>
   )
