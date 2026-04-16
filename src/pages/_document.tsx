56a3d73a82929b866b1649d6b78eb42fbf7efd57
Lin <tzu.hsun.lin046@gmail.com>
2026-04-16 12:59:44 +0800

    chore: remove legacy mobile web app meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,6 @@ export default function Document() {
 
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#fafafa" />
-        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Closy" />
         <link rel="apple-touch-icon" href="/icons/apple-icon-180.png" />
