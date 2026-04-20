d39dcc566133050ba5208f6a9f24ef77e7a45756
Lin <tzu.hsun.lin046@gmail.com>
2026-04-20 11:23:00 +0800

    update: refine outfit adjustment result flow timing

--- src/modules/home/components/outfit-adjust-drawer/OutfitAdjustResultView.tsx
+++ src/modules/home/components/outfit-adjust-drawer/OutfitAdjustResultView.tsx
@@ -1,6 +1,6 @@
-import type { AdjustStreamResult } from '../../types/outfitAdjustChat'
 import { AdjustSuggestionChip } from './AdjustSuggestionChip'
 import { OutfitComparisonSection } from './OutfitComparisonSection'
+import type { AdjustStreamResult } from '../../types/outfitAdjustChat'
 
 type OutfitAdjustResultViewProps = {
   result: AdjustStreamResult
@@ -8,7 +8,11 @@ type OutfitAdjustResultViewProps = {
   onRevert: () => void
 }
 
-export const OutfitAdjustResultView = ({ result, onConfirm, onRevert }: OutfitAdjustResultViewProps) => {
+export const OutfitAdjustResultView = ({
+  result,
+  onConfirm,
+  onRevert,
+}: OutfitAdjustResultViewProps) => {
   return (
     <div className="hide-scrollbar flex min-h-0 flex-1 flex-col gap-6 overflow-y-auto">
       <p className="font-label-xxl text-center text-neutral-800">已為您調整穿搭</p>
@@ -20,8 +24,8 @@ export const OutfitAdjustResultView = ({ result, onConfirm, onRevert }: OutfitAd
       </div>
       <div className="hide-scrollbar min-w-0 shrink-0 overflow-x-auto">
         <div className="flex gap-2">
-          <AdjustSuggestionChip label="我喜歡！幫我換成這套" onClick={onConfirm} />
           <AdjustSuggestionChip label="還是原本那套好了" onClick={onRevert} />
+          <AdjustSuggestionChip label="我喜歡！幫我換成這套" onClick={onConfirm} />
         </div>
       </div>
     </div>
